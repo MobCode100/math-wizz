ac69226aa2b5e413557ef79657c446dd101668df
Mob Code 100 <dastur07@yandex.com>
2024-09-23 20:19:13 +0800

    Improve UI & Button

---
+++ scenes/UI element/button.gd
@@ -0,0 +1,39 @@
+extends Button
+
+var hovered = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$".".custom_minimum_size = $".".size
+	$RichTextLabel.text = "[center]" + $".".text + "[/center]"
+	$".".text = ""
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_down() -> void:
+	print($RichTextLabel.position.y)
+	$RichTextLabel.position.y += 3
+	return
+
+func _on_button_up() -> void:
+	if(hovered):
+		$RichTextLabel.position.y -= 3
+	return
+
+func _on_mouse_entered() -> void:
+	if(button_pressed):
+		$RichTextLabel.position.y += 3
+	$RichTextLabel.position.y += 1
+	hovered = true
+	return
+
+func _on_mouse_exited() -> void:
+	if(button_pressed):
+		$RichTextLabel.position.y -= 3
+	$RichTextLabel.position.y -= 1
+	hovered = false
+	return
